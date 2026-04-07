28b137975ec76f53c1867ef6840c2e39edd4fefb
Jason Barron <jasonbarronyou222@gmail.com>
2026-04-07 14:03:01 +0200

    fixture-added

---
+++ Playwright-Scripts/Fixture/athenticated-state-logged-in.ts
@@ -0,0 +1,18 @@
+import {test as base, expect, Page} from '@playwright/test'
+ 
+type myFixture = {
+    loggedinState: Page;
+}
+ 
+export const test = base.extend<myFixture>({loggedinState: async({page},use)=>{
+
+    await page.goto('https://quality-engineering-labs.vercel.app/login.html')
+    await page.getByTestId('login-username').fill('admin')
+    await page.getByTestId('login-password').fill('password123')
+    await page.getByTestId('login-submit').click()
+    await expect(page.getByTestId('dashboard-section')).toBeVisible()
+    await use(page)
+
+}})
+
+export {expect}
