49dc06b2219daaf3f0d1547e76f1e15fda1d166b
Jason Barron <jasonbarronyou222@gmail.com>
2026-04-08 12:56:24 +0200

    comments added to anayltics check - code updated

--- Playwright-Scripts/tests/Cross-Role Syncronisation/admin-checks-analytics.spec.ts
+++ Playwright-Scripts/tests/Cross-Role Syncronisation/admin-checks-analytics.spec.ts
@@ -4,6 +4,9 @@ const baseURL: string = 'https://quality-engineering-labs.vercel.app/'
 
 test('admin checks analytics', async ({page})=>{
     await page.goto(`${baseURL}login.html`)
+    await page.getByLabel('Username').fill('admin')
+    await page.getByLabel('Password').fill('password123')
+    await page.getByRole('button', {name: 'Sign In'}).click()
     await page.getByTestId('action-analytics').click()
-    await expect (page).toHaveTitle('Business Analytics')
+    await expect (page.getByText('Business Analytics')).toBeVisible()
 })
